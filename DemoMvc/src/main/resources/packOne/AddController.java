e246d13001685095a5b70dac581c2ca71dd843a5
Minhazur Rahman <rahmanole.bd@gmail.com>
2019-04-07 23:32:33 +0600

    up

---
+++ DemoMvc/src/main/resources/packOne/AddController.java
@@ -0,0 +1,14 @@
+package packOne;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public void add() {
+		
+		System.out.println("Inside add controller");
+	}
+}
